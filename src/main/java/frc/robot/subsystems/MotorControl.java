6227886170b7f9fae2b96b005c01a226fe5fd8fc
2130Silber <65629863+BrodieBecker@users.noreply.github.com>
2024-01-20 14:17:34 -0800

    Inverted motor directions on launcher

--- src/main/java/frc/robot/subsystems/MotorControl.java
+++ src/main/java/frc/robot/subsystems/MotorControl.java
@@ -12,8 +12,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class MotorControl extends SubsystemBase {
-  private final CANSparkMax motor1 = new CANSparkMax(0, MotorType.kBrushed), motor2 = new CANSparkMax(1, MotorType.kBrushed);
-  private final VictorSP motor3 = new VictorSP(2), motor4 = new VictorSP(3);
+  private final CANSparkMax motor1 = new CANSparkMax(10, MotorType.kBrushed), motor2 = new CANSparkMax(11, MotorType.kBrushed);
+  private final VictorSP motor3 = new VictorSP(3), motor4 = new VictorSP(5);
   
   /** Creates a new motorControl. */
   public MotorControl() {
@@ -25,9 +25,9 @@ public class MotorControl extends SubsystemBase {
     // This method will be called once per scheduler run
     double power = SmartDashboard.getNumber(getName(), 0);
 
-    motor1.set(power);
-    motor2.set(power);
-    motor3.set(power);
-    motor4.set(power);
+    motor1.set(power * -1);
+    motor2.set(power * -1);
+    motor3.set(power * 1);
+    motor4.set(power * 1);
   }
 }
