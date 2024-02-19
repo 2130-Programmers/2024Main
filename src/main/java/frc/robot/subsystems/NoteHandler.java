3262a06ecd714500c442425c2618267385b08ae8
2130Silber <65629863+BrodieBecker@users.noreply.github.com>
2024-02-19 14:30:18 -0800

    Added additional functionality to NoteHandler

--- src/main/java/frc/robot/subsystems/NoteHandler.java
+++ src/main/java/frc/robot/subsystems/NoteHandler.java
@@ -10,6 +10,7 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class NoteHandler extends SubsystemBase {
   private static final DigitalInput
@@ -27,7 +28,29 @@ public class NoteHandler extends SubsystemBase {
   intakeBottom = new CANSparkMax(0, MotorType.kBrushless);
 
   /** Creates a new NoteHandler. */
-  public NoteHandler() {}
+  public NoteHandler() {
+  }
+
+  /**
+   * Move the launcher to specified angle: higher values move the intake higher, and thus the launcher points lower. 0 is the resting position and used for picking up nots off the ground
+   * @param angle - the angle to move the launcher to, expressed in encoder ticks
+   * @return true when the launcher has reached its target
+   */
+  public boolean moveToAngle(double angle) {
+    if(angle > Constants.LauncherConstants.LAUNCHER_MAX_ANGLE) {
+      throw new Error("Target launcher angle out of bounds");
+    }
+
+    double angleError = (rotateLeft.getPosition().getValueAsDouble() + rotateRight.getPosition().getValueAsDouble())/2;
+
+    if(Math.abs(angleError) > Constants.LauncherConstants.ANGLE_DEADZONE) {
+      setRotatePower(angleError * Constants.LauncherConstants.ANGLE_POWER);
+      return true;
+    }else{
+      setRotatePower(0);
+      return false;
+    }
+  }
 
   /**
    * Move intake to bottom position and zero encoder
@@ -57,16 +80,28 @@ public class NoteHandler extends SubsystemBase {
     return noteLaserEye.get();
   }
 
+  /**
+   * Set the power of intake motors - positive moves the note in
+   * @param power - double, from -1 to 1
+   */
   private void setIntakePower(double power) {
     intakeTop.set(power);
     intakeBottom.set(power);
   }
 
+    /**
+   * Set the power of launcher motors - positive moves the note in
+   * @param power - double, from -1 to 1
+   */
   public void setLaunchPower(double power) {
     launcherTop.set(power);
     launcherBottom.set(power);
   }
 
+    /**
+   * Set the power of rotation motors - positive moves the intake side up, and thus the launcher points lower.
+   * @param power - double, from -1 to 1
+   */
   private void setRotatePower(double power) {
     rotateLeft.set(power);
     rotateRight.set(power);
